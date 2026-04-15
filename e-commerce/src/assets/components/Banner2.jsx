6e25928beb6e390f684b6690852ac2c66d90a312
Ayan-x4 <mdayan6190@gmail.com>
2026-04-15 22:42:36 +0530

    Add another banner on landing page

---
+++ e-commerce/src/assets/components/Banner2.jsx
@@ -0,0 +1,14 @@
+
+
+const Banner2 = () => {
+  return (
+    <div className="flex  items-center justify-center  md:rounded-2xl  rounded-xl overflow-clip cursor-pointer border-2 border-white  hover:border-[#008ECC] duration-1000">
+    <div className="md:h-[200px] h-[150px]  md:w-[430px] w-[367px]  flex items-center cover">
+    <img src="https://rukminim2.flixcart.com/fk-p-flap/3200/2000/image/6c5a5d57bb9d3f8a.jpg?q=60" alt="hhh"
+    className="h-full w-full "></img>
+    </div>
+    </div>
+  )
+}
+
+export default Banner2
